e62d0222db6feb584df95a88a1318ec172becdd2
yokesh-a <bossyokesh3377@gmail.com>
2020-07-25 22:33:30 +0530

    initial

---
+++ api/index.php
@@ -0,0 +1,33 @@
+<?php
+
+//super admin details
+
+$userid = "admin";
+$password = "admin";
+
+session_start();
+
+function notify($msg) {
+    echo "<script>notify('".$msg."');</script>";
+  }
+if(isset($_POST['__a'])){
+
+    if($_POST['__a'] == 'login'){
+        if($_POST['user'] == $userid AND $_POST['password'] == $password){
+            $_SESSION["log"]="open";
+        }else{
+            notify("Wrong Creditials");
+        }
+    }
+    
+        if(isset($_SESSION["log"])){
+            include 'system/superadmin/dashboard.php';
+        }else{
+            include 'system/login/loginpage.php';
+        }
+}else{
+    header("Location: /");
+}
+
+
+?>
