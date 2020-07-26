679458f13bd4f71e212b624f71a08fb09b54b172
yokesh-a <bossyokesh3377@gmail.com>
2020-07-26 11:29:15 +0530

    try

--- api/system/login/loginpage.php
+++ api/system/login/loginpage.php
@@ -191,7 +191,6 @@ body {
 </div>
 </div>
 <div id="snackbar"></div>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script>
 function notify(data) {
   var x = document.getElementById("snackbar");
