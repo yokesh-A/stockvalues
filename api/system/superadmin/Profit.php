7260fb20e42a2495393743509b06a7c5a4738d74
yokesh-a <bossyokesh3377@gmail.com>
2020-08-23 00:18:16 +0530

    yokesh

--- api/system/superadmin/Profit.php
+++ api/system/superadmin/Profit.php
@@ -1,6 +1,7 @@
 <?php if( $_SESSION["user"] === "superadmin"){ ?>
 
 <h1>Profit Boards</h1>
+<button onclick="window.location.href='/api/?__a=backup';">Back All</button>
 
 <?php }else{
     echo "<h1>Session Expired</h1><h3>Page Not Found</h3>";
---
+++ api/system/superadmin/backupdb.php
@@ -0,0 +1,14 @@
+<?php
+
+$file = "system/2cheap.db";
+
+header('Content-Description: File Transfer');
+header('Content-Disposition: attachment; filename=2cheap.nutz');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($file));
+header("Content-Type: text/plain");
+readfile($file);
+
+?>
