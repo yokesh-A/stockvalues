8b1bfabd454d7a0b5f21cc653b2353df046b16ad
yokesh-a <bossyokesh3377@gmail.com>
2020-08-28 11:40:35 +0530

    yokesh

--- api/system/superadmin/Profit.php
+++ api/system/superadmin/Profit.php
@@ -1,3 +1,10 @@
+<style>
+.dataTables_filter{
+    float:right;
+    margin-right:15%;
+}
+</style>
+
 <div class="row"><div class="col-sm-12">
 <?php if( $_SESSION["user"] === "superadmin"){ ?>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/se-2.2.13/jq-3.3.1/dt-1.10.21/af-2.3.5/b-1.6.3/b-html5-1.6.3/b-print-1.6.3/cr-1.5.2/kt-2.5.2/r-2.2.5/sc-2.0.2/sp-1.1.1/datatables.min.css"/>
